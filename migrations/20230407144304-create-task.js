db7038685383ae8807565be87caccfaecb5bc3b7
Bohdan Tertyshchev <bobterya@gmail.com>
2023-04-07 17:52:11 +0300

    task resolve

---
+++ migrations/20230407144304-create-task.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isDone: {
+        field: 'is_done',
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      deadline: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        field: 'create_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  }
+};
