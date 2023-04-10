c9f0dd3b6128161704d61afced4270defe63b034
Bohdan Tertyshchev <bobterya@gmail.com>
2023-04-10 23:06:52 +0300

    get users with groups

--- controllers/Group.controller.js
+++ controllers/Group.controller.js
@@ -1,4 +1,4 @@
-const {Group} = require('../models/Group');
+const {Group} = require('../models/');
 
 module.exports.createGroup = async(req, res, next) => {
     try {
@@ -16,6 +16,16 @@ module.exports.addUserToGroup = async(req, res, next) => {
         const groupInstance = await Group.findByPk(groupId);
         const result = await groupInstance.addUser(userInstance);
         return res.status(200).send('User successfully added to group');
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.getUserGroups = async(req, res, next) => {
+    try {
+        const {userInstance} = req;
+        const groups = await userInstance.getGroups();
+        return res.status(200).send(groups);
     } catch (error) {
         next(error);
     }
